a30e35b4df30892c88891615eaf3b1cf98592d76
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-10-21 07:16:29 -0300

    subindo versao final

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -1,31 +1,33 @@
-import {AiFillPhone, AiOutlineMail, AiFillEnvironment} from 'react-icons/ai'
+import { AiFillPhone, AiFillEnvironment } from 'react-icons/ai'
+
+import { MdOutlineMailOutline } from 'react-icons/md'
 
 import '../styles/components/informationcontainer.scss'
 
 function InformationContainer() {
-  return <section id="information">
-    <div className="info-card">
-        <AiFillPhone id="phone-icon"/>
-        <div>
-            <h3>Telefone</h3>
-            <p>(35)99948-2180</p>
+    return <section id="information">
+        <div className="info-card">
+            <AiFillPhone id="phone-icon" />
+            <div>
+                <h3>Telefone</h3>
+                <p>(35)99948-2180</p>
+            </div>
         </div>
-    </div>
-    <div className="info-card">
-        <AiOutlineMail id="email-icon"/>
-        <div>
-            <h3>E-mail</h3>
-            <p>pcastro@sga.pucminas.br</p>
+        <div className="info-card">
+            <AiFillEnvironment id="pin-icon" />
+            <div>
+                <h3>Localização</h3>
+                <p>Belo Horizonte/MG</p>
+            </div>
         </div>
-    </div>
-    <div className="info-card">
-        <AiFillEnvironment id="pin-icon"/>
-        <div>
-            <h3>Localização</h3>
-            <p>Belo Horizonte/MG</p>
+        <div className="info-card">
+            <MdOutlineMailOutline id="email-icon" />
+            <div>
+                <h3>E-mail</h3>
+                <p>pcastro@sga.pucminas.br</p>
+            </div>
         </div>
-    </div> 
-  </section>
+    </section>
 }
 
 export default InformationContainer
